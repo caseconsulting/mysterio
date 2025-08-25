c633769d2d11dadc01a8c2961f36a6e209fce397
logburn <21106956+logburn@users.noreply.github.com>
2025-08-25 13:49:06 -0400

    POR-3274: put back old method

--- get-timesheet-data/unanet-timesheets.js
+++ get-timesheet-data/unanet-timesheets.js
@@ -159,10 +159,8 @@ async function getTimesheet(startDate, endDate, title, userId) {
 
       // add the hours worked for the project
       let jobCode = getProjectName(slip.project.name);
-      job = {};
-      job[jobCode] ??= 0;
-      job[jobCode] += hoursToSeconds(Number(slip.hoursWorked));
-      timesheets.push(job);
+      timesheet.timesheets[jobCode] ??= 0;
+      timesheet.timesheets[jobCode] += hoursToSeconds(Number(slip.hoursWorked));
 
       // add bill code to non-billables if it is not marked as billable
       if (!BILLABLE_CODES.includes(slip.projectType.name)) {
